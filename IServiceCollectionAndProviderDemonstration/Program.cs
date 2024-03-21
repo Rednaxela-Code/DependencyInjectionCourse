1abe2b92f4164188436b0739aad0814bf7a002c8
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-03-21 15:37:28 +0100

    New Chapter - Dependency Injection Container
    IServiceCollection and IServerProvider

---
+++ IServiceCollectionAndProviderDemonstration/Program.cs
@@ -0,0 +1,56 @@
+﻿IDataAccess dataAccess = new DataAccess();
+IBusiness biz = new Business(dataAccess);
+UserInterface ui = new UserInterface(biz);
+
+ui.Signup();
+
+public class UserInterface
+{
+    private IBusiness _business;
+
+    public UserInterface(IBusiness business)
+    {
+        _business = business;
+    }
+
+    public void Signup()
+    {
+        Console.Write("Enter User Name:");
+        string _userName = Console.ReadLine();
+        Console.Write("Enter Password:");
+        string _password = Console.ReadLine();
+        _business.Signup(_userName, _password);
+    }
+}
+
+public interface IBusiness
+{
+    public void Signup(string userName, string password);
+}
+
+public class Business : IBusiness
+{
+    private IDataAccess _access;
+
+    public Business(IDataAccess access)
+    {
+        _access = access;
+    }
+    public void Signup(string userName, string password)
+    {
+        _access.Signup(userName, password);
+    }
+}
+
+public interface IDataAccess
+{
+    public void Signup(string userName, string password);
+}
+
+public class DataAccess : IDataAccess
+{
+    public void Signup(string userName, string password)
+    {
+        // Use EF to write data into SQL Server
+    }
+}
