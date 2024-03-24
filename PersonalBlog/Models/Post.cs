8ed822fb5012de8b03c8d35e81ebfff58d5169e2
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-03-24 12:50:12 +0100

    Adding Model Contents

--- PersonalBlog/Models/Post.cs
+++ PersonalBlog/Models/Post.cs
@@ -1,6 +1,19 @@
-﻿namespace PersonalBlog.Models
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace PersonalBlog.Models
 {
+    [DynamoDBTable(tableName: "post")]
     public class Post
     {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        public DateTime PostDateTime { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Post()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
     }
 }
